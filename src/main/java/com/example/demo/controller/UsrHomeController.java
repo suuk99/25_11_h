46bb0ea16c156155a0062682503a26941b16e10b
devCJH <http-@naver.com>
2025-11-04 11:53:26 +0900

    jasper, servlet, jsp사용

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -2,15 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	
 	@GetMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
-		return "안녕하세요";
+		return "usr/home/main";
 	}
 	
 }
